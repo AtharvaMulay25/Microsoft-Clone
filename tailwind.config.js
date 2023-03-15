40fc85702a8eba61f6bd9d295148fa412d784988
Atharva Suhas Mulay <atharvamulay25@gmail.com>
2023-03-16 02:38:04 +0530

    updated src

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens:
+      {
+        'md': '975px'
+      },
+      colors: {
+        hyperlinkColor: '#206ab8',
+      }
+    },
+  },
+  plugins: [],
+}
